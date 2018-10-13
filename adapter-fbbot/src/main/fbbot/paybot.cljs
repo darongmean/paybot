52861605ca294f9a6b97b6b4b51843eeae61b25d
Darong Mean <darong012@gmail.com>
2018-10-13 11:56:19 +0700

    Create an endpoint to connect with FB Messenger
    Note: `body` and `headers` are required to make
    `wrap-restful-format` working properly.

--- adapter-fbbot/src/main/fbbot/paybot.cljs
+++ adapter-fbbot/src/main/fbbot/paybot.cljs
@@ -1,27 +1,42 @@
 (ns fbbot.paybot
   (:require
-    [macchiato.middleware.defaults :as defaults]
-    [macchiato.util.response :as res]
+    [macchiato.middleware.defaults :as middleware]
+    [macchiato.middleware.restful-format :as rest-middleware]
+    [macchiato.util.response :as ring-response]
 
     [reitit.ring :as ring]
 
     [taoensso.timbre :refer-macros [info]]))
 
 
-(defn hello-world [request response raise]
+(defn hello-world [request respond raise]
   (-> (str "Hello world!")
-      (res/ok)
-      (res/content-type "text/plain")
-      (response)))
+      (ring-response/ok)
+      (ring-response/content-type "text/plain")
+      (respond)))
+
+
+(defn messenger-response [request respond raise]
+  (-> {:message "Hello FB Messenger!"}
+      (ring-response/ok)
+      (respond)))
 
 
 (def paybot
   (ring/ring-handler
     (ring/router
-      [["/" {:middleware [[defaults/wrap-defaults defaults/site-defaults]]
-             :get        hello-world}]])
+      [["/" {:middleware [[middleware/wrap-defaults middleware/site-defaults]]
+             :get        hello-world}]
+       ["/messenger" {:middleware [[middleware/wrap-defaults middleware/api-defaults]
+                                   [rest-middleware/wrap-restful-format]]
+                      :get        messenger-response}]])
     (ring/create-default-handler)))
 
-(comment
-  (do
-    (paybot {:request-method :get, :uri "/"} cljs.pprint/pprint identity)))
+(comment (do
+           (paybot
+             {:request-method :get
+              :headers        {"Accept" "application/json"}
+              :body           {}
+              :uri            "/messenger"}
+             cljs.pprint/pprint
+             identity)))
