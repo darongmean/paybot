f4872110cdb510ec7f1490382c8a14d79567e14e
Darong Mean <darong012@gmail.com>
2018-10-13 16:58:21 +0700

    Convert example test to generative test

--- adapter-fbbot/src/main/fbbot/paybot.cljs
+++ adapter-fbbot/src/main/fbbot/paybot.cljs
@@ -1,7 +1,11 @@
 (ns fbbot.paybot
   (:require
+    [com.gfredericks.test.chuck.clojure-test :refer-macros [checking]]
+
     [cljs.spec.alpha :as s]
     [cljs.test :refer (deftest is)]
+
+    [clojure.test.check.generators :as gen]
 
     [macchiato.middleware.defaults :as middleware]
     [macchiato.middleware.restful-format :as rest-middleware]
@@ -34,23 +38,6 @@
         (respond))))
 
 
-(declare paybot)
-
-(deftest verify-webhook-by-returning-provided-challenge
-  (paybot
-    {:request-method :get
-     :headers        {"Accept" "application/json"}
-     :body           {}
-     :params         {"hub.mode"         "subscribe",
-                      "hub.challenge"    "800315870",
-                      "hub.verify_token" "MAGIC_TOKEN_1234"}
-     :uri            "/messenger"}
-    (fn [response]
-      (is (= 200 (:status response)))
-      (is (= "application/json" (get-in response [:headers "Content-Type"])))
-      (is (= "\"800315870\"" (:body response))))
-    identity))
-
 (def paybot
   (ring/ring-handler
     (ring/router
@@ -60,6 +47,24 @@
                                    [rest-middleware/wrap-restful-format {:keywordize? true}]]
                       :get        subscribe-response}]])
     (ring/create-default-handler)))
+
+(deftest paybot-test
+  (checking "that the same 'challenge' token returned when verify webhook" 100
+    [challenge gen/string-alphanumeric
+     verify-token gen/string-alphanumeric]
+    (paybot
+      {:request-method :get
+       :headers        {"Accept" "application/json"}
+       :body           {}
+       :params         {"hub.mode"         "subscribe",
+                        "hub.challenge"    challenge,
+                        "hub.verify_token" verify-token}
+       :uri            "/messenger"}
+      (fn [response]
+        (is (= 200 (:status response)))
+        (is (= "application/json" (get-in response [:headers "Content-Type"])))
+        (is (= (str "\"" challenge "\"") (:body response))))
+      identity)))
 
 (comment
   (defn success-debug [obj]
